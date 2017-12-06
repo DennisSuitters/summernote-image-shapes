179bca29781a1b28e4ce434c859e7ebf6eecf980
root <dennis@studiojunkyard.com>
2017-12-07 01:38:19 +1100

    Fix Formatting

--- summernote-image-shapes.js
+++ summernote-image-shapes.js
@@ -2,7 +2,7 @@
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     define(['jquery'],factory)
-  } else if (typeof module === 'object'&&module.exports) {
+  } else if (typeof module === 'object' && module.exports) {
     module.exports = factory(require('jquery'));
   } else {
     factory(window.jQuery)
